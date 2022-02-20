0bd37b4e929dbd2f1e35cdca386ee6a91c0f210d
Josh Talley <jtdev84@gmail.com>
2022-02-20 14:44:08 +0000

    Add edit template and update action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
+  resources :users
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
 end
