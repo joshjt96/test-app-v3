db676ce988a0e08e5efa213fffdaaf73832c8f59
Josh Talley <jtdev84@gmail.com>
2022-02-21 12:59:02 +0000

    Change articles controller and _article from @articles to @article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles or /articles.json
   def index
-    @articles = Article.all
+    @article = Article.all
   end
 
   # GET /articles/1 or /articles/1.json
