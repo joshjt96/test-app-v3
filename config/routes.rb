f86b2a3b1fee8db950bd24307a3bc4fa93e51bce
Josh Talley <jtdev84@gmail.com>
2022-02-22 19:17:04 +0000

    More user settings/actions, restrict access etc

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :users
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
