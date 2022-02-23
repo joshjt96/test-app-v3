ec2e67e42c3e2551445942eca1ac458de430efcc
Josh Talley <jtdev84@gmail.com>
2022-02-23 16:12:01 +0000

    Update name & CSS

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
-  resources :users
   get 'signup', to: 'users#new'
-  post 'users', to: 'users#create'
+  resources :users, except: [:new]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  resources :categories, except: [:destroy]
 end
