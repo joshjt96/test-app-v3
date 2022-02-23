99168396eb0d1c7c35dd92e7c7f02a1899902dc0
Josh Talley <jtdev84@gmail.com>
2022-02-23 12:42:34 +0000

    App deployment ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: %i[ show edit update]
+    before_action :set_user, only: %i[ show edit update destroy]
     before_action :require_user, only: %i[ edit update ]
-    before_action :require_same_user, only: %i[ edit update ]
+    before_action :require_same_user, only: %i[ edit update destroy ]
     
     def show
          @articles = @user.articles
@@ -37,6 +37,13 @@ class UsersController < ApplicationController
           render 'new'
       end
     end
+    
+    def destroy
+        @user.destroy
+        session[:user_id] = nil if @user == current_user
+        flash[:notice] = "Account and all associated articles have been deleted."
+        redirect_to root_path
+    end
 
    private
    def user_params
@@ -48,7 +55,7 @@ class UsersController < ApplicationController
    end
    
    def require_same_user
-      if current_user != @user
+      if current_user != @user && !current_user.admin?
         flash[:alert] = "You can only edit or delete your own profile."
         redirect_to @user
       end
