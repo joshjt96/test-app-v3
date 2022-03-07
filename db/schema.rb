4afeb65e84e75499c66334e4e26ae111da112ce2
Josh Talley <jtdev84@gmail.com>
2022-03-07 16:32:35 +0000

    Implement many to many association with article categories table, add association from frontend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_175908) do
+ActiveRecord::Schema.define(version: 2022_03_07_145334) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
