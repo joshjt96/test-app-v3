b9224c7a9d27688be1d8493574d2cc41f6f79d74
Josh Talley <jtdev84@gmail.com>
2022-03-08 14:34:28 +0000

    Fix dropdown issue, complete all category functionality and update views.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,6 +7,7 @@ end
 
 def show 
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(:page => params[:page], :per_page => 5)
 end 
 
 def index 
@@ -22,6 +23,20 @@ def create
          render 'new'
     end
 end 
+
+def edit
+  @category = Category.find(params[:id])
+end
+
+def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name was successfully updated"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+end
 
   private
 
